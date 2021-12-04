dd46b3d663e0b5feb5387a719b909c33b1c648f8
bryan mcwatters <bmcwatters@comcast.net>
2021-12-04 17:38:27 -0500

    init commit

---
+++ SelectionStatements2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Choose a school subject");
+string userSubject = Console.ReadLine();
+
+switch (userSubject)
+{
+    case "English":
+        Console.WriteLine("Welcome to English");
+        break;
+    case "Math":
+        Console.WriteLine("Welcome to Math");
+        break;
+    case "Science":
+        Console.WriteLine("Welcome to Science");
+        break;
+    case "History":
+        Console.WriteLine("Welcome to History");
+        break;
+
+    default:
+        Console.WriteLine($"We don't offer {userSubject} here");
+        break;
+}
